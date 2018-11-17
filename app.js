c68d6a9e52f74c2254367d76875bfcf827d344fb
rihaz_22309 <rihaz.zahir@paytm.com>
2018-11-17 13:44:54 +0530

    final commit

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const app = express();
 const PORT = parseInt(process.env.PORT, 10) | 3003;
 
 const bodyParser = require('body-parser');
+const request = require('request');
+var apiKey = process.env.APIKEY;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
-    res.render('index');
+    res.render('index', {weather: null, error: null});
 });
 
 app.post('/', (req, res)=>{
-    console.log(req.body.city);
 
-    res.render('index');
+    var city = req.body.city;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    request(url, function(err, response, body){
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'})
+        }
+        else{
+            var weather = JSON.parse(body);
+            if(weather.main == undefined){
+                res.render('index', {weather: null, error: 'Error, please try again'});
+            }
+            else{
+                var weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
 });
 
 app.listen(PORT, ()=>{
-    console.log("server is listenting to ", PORT);
+    console.log("server is listenting to ", PORT, " apikey ", apiKey);
 });
